1b61326625d4e5239541657a48719850c9664453
Sergey Lozhkin <sergey.lozhkin@brightpattern.com>
2020-10-13 13:16:17 -0700

    lucid // ark site as an example

--- Main.hs
+++ Main.hs
@@ -1,6 +1,41 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ExtendedDefaultRules #-}
+
 module Main where
 
+import Prelude hiding (writeFile)
 import Lucid
+import Data.Text.Lazy.IO
 
 main :: IO ()
-main = putStrLn "RTM Specification"
+main = do
+  writeFile "ActivitiesHistory.html" $ renderText activitiesHistory
+  -- writeFile "index.html" $ renderText index
+  -- print index
+
+activitiesHistory :: Html ()
+activitiesHistory = do
+  doctype_
+  html_ [lang_ "en"] $ do
+    head_ $ do
+      title_ "Ark | Activities | History"
+      meta_ [charset_ "UTF-8"]
+      meta_ [httpEquiv_ "X-UA-Compatible", content_ "IE=edge"]
+      link_ [rel_ "stylesheet", href_ "css/fonts.css", type_ "text/css", charset_ "utf-8"]
+      link_ [rel_ "stylesheet", href_ "css/bootstrap.min.css", type_ "text/css", charset_ "utf-8"]
+      script_ [type_ "text/javascript", src_ "js/elm-runtime.min.js"] ""
+      script_ [type_ "text/javascript", src_ "js/ActivitiesHistory.min.js"] ""
+      style_ $
+        "a:link{color:inherit;text-decoration:inherit;}" <>
+        "a:visited{color:inherit;text-decoration:inherit;}" <>
+        "a:hover{color:inherit;text-decoration:inherit;}" <>
+        "a:active{color:inherit;text-decoration:inherit;}" <>
+        "a{outline-style:none;outline-width:0px;}" <>
+        ".zero-border{padding:0px;border-width:0px;margin:0px;}"
+    body_ $ do
+      noscript_ "Please, enable JavaScript"
+      script_ [type_ "text/javascript"] "Elm.fullscreen(Elm.ActivitiesHistory);"
+
+-- index :: Html ()
+-- index = do
+--   doctype_
