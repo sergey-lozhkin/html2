5665c0a9ef5dd9d0d12cf596e37ee085e096b783
Sergey Lozhkin <sergey.lozhkin@brightpattern.com>
2020-10-13 13:16:29 -0700

    lucid: background

---
+++ background.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ExtendedDefaultRules #-}
+
+module Main where
+
+import Prelude hiding (writeFile)
+import Lucid
+import Lucid.Base
+import Data.Text (Text, unpack)
+import Data.Text.Lazy.IO (writeFile)
+
+dataRide_ :: Text -> Attribute
+dataRide_ = makeAttribute "data-ride"
+
+dataInterval_ :: Text -> Attribute
+dataInterval_ = makeAttribute "data-interval"
+
+dataSlide_ :: Text -> Attribute
+dataSlide_ = makeAttribute "data-slide"
+
+main :: IO ()
+main = do
+  writeFile "html/background.html" $ renderText $ do
+    doctype_
+    -- html_ [lang_ "en"] $ do
+    html_ $ do
+      head_ $ do
+        title_ "Background"
+        meta_ [charset_ "UTF-8"]
+        meta_ [httpEquiv_ "X-UA-Compatible", content_ "IE=edge"]
+        link_ [rel_ "stylesheet", type_ "text/css", charset_ "utf-8", href_ "css/fonts.css"]
+        link_ [rel_ "stylesheet", type_ "text/css", charset_ "utf-8", href_ "css/bootstrap.min.css"]
+        style_ $
+          "a:link{color:inherit;text-decoration:inherit;}" <>
+          "a:visited{color:inherit;text-decoration:inherit;}" <>
+          "a:hover{color:inherit;text-decoration:inherit;}" <>
+          "a:active{color:inherit;text-decoration:inherit;}" <>
+          "a{outline-style:none;outline-width:0px;}" <>
+          ".nil{padding:0px;border-width:0px;margin:0px;}"
+      body_ [class_ "nil"] $ do
+        div_ [style_
+            $   "margin:2em;"
+            <>  "border:2em solid lightblue;"
+            <>  "padding:2em;"
+            <>  "background:pink;"
+            <>  "background-clip:padding-box;"] $ do
+          "Hola, Mundo!"
